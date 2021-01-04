95030a23c28531aac559903889021a4176707948
Павел Жуков <33721692+LeaveMyYard@users.noreply.github.com>
2021-01-04 18:05:05 +0200

    Update context_editor.py

--- bine/context_editor.py
+++ bine/context_editor.py
@@ -13,6 +13,15 @@ class BineContextEditor:
         self.db_handler = db_handler
         self._last_block_number: int = 0
 
+    @property
+    def last_block_number(self) -> int:
+        return self._last_block_number
+
+    def add_block(self) -> None:
+        self._last_block_number += 1
+        self.db_handler.set_last_block(self.__class__.__name__, self.last_block_number)
+        self.db_handler.commit()
+
     def create_item(self, owner: str, amount: int, item_id: int) -> None:
         self.transfer_item(owner, amount, 0x0, item_id)
 
@@ -29,14 +38,18 @@ class BineContextEditor:
             self.db_handler.items.remove(_id, _from, amount)
         if int(to, base=16) != 0x0:
             self.db_handler.items.add(_id, to, amount)
+        self.db_handler.commit()
 
     def place_lot(self, owner: str, item_id: int, price: int, amount: int) -> int:
         self.lot_id += 1
         self.db_handler.market.add_lot_data(self.lot_id, owner, item_id, price, amount)
+        self.db_handler.commit()
         return self.lot_id
 
     def buy_lot(self, lot_id: int, buyer: str, amount: int) -> None:
         self.db_handler.market.add_lot_bought_data(lot_id, buyer, amount)
+        self.db_handler.commit()
 
     def cancel_lot(self, lot_id: int) -> None:
         self.db_handler.market.add_lot_canceled(lot_id)
+        self.db_handler.commit()
